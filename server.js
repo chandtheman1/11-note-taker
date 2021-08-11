4ead3e6772f07227749eba2a5aeff27bd3c8d183
Chandler Liang <chandler.liang123@gmail.com>
2021-08-11 14:41:42 +1000

    added post route for creating notes

--- server.js
+++ server.js
@@ -1,10 +1,79 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const util = require('util');
+// const db = require('./db/db.json');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.static('public'));
+
+const readFromFile = util.promisify(fs.readFile);
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(content);
+        writeToFile(file, parsedData);
+      }
+    });
+};
+
+
+//GET routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+});
+
+app.get('/api/notes', (req, res) => {
+
+    console.info(`${req.method} request received for notes`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+
+});
+
+app.post('/api/notes', (req, res) => {
+    console.log("hello");
+    const { title, text } = req.body;
+
+    if (req.body) {
+
+        const newNote = {
+            title,
+            text,
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json('Note added');
+    } else {
+        res.error('Error in adding note');
+    }
+
+});
+
+
+//wildcard route
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
